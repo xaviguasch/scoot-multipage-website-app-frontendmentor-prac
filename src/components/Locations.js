9768ae28ba9c25f4487f5445973ea20c973c2465
xaviguasch <xaviguasch@gmail.com>
2022-02-20 17:16:47 +0100

    added missing markup for Locations component

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -2,20 +2,29 @@ import React from 'react'
 
 import './Locations.css'
 
+import worldMap from '../assets/images/world-map-mobile.png'
+
 const Locations = () => {
   return (
     <div className='Locations'>
-      <section className='banner'>
+      <section className='banner banner--careers'>
         <h2 className='title'>Locations</h2>
       </section>
       <section className='main'>
-        <div className='main__e'>
-          <div className='map-illus'>
-            <img src='' alt='' />
+        <div className='main__f'>
+          <div className='main__f__world'>
+            <img src={worldMap} alt='world map' />
+
+            <ul className='mobile-map-cities-list'>
+              <li className='city'>New York</li>
+              <li className='city'>London</li>
+              <li className='city'>Jakarta</li>
+              <li className='city'>Yokohama</li>
+            </ul>
           </div>
 
-          <div className='main__e__copy'>
-            <h2 className='title title--md'>Your city not listed?</h2>
+          <div className='main__f__copy'>
+            <h2 className='title title--md'>Your City Not listed?</h2>
             <p className='text text--alt'>
               If you’d like to see Scoot in your hometown, be sure to let us know. We
               track requests and plan launches based on demand. Feel free to message us by
